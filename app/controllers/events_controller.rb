a2c990f16280aca2aa1c3586fd90ac59a092db63
curly08 <mattynoakes08@gmail.com>
2021-12-07 11:09:36 -0500

    Add #new, #create, and #event_params to Events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,31 @@
 class EventsController < ApplicationController
+  # before_action :authenticate_user!, only: %i[show new create]
+
   def index
     @events = Event.all
+  end
+
+  def new
+    @event = current_user.events.build
+  end
+
+  def create
+    @event = current_user.events.build(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @post, notice: 'Event was successfully created.' }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :date, :location)
   end
 end
