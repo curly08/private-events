16b9aaca45e59a5d6faacd6ad2ac5dfa9aaeaf20
curly08 <mattynoakes08@gmail.com>
2021-12-08 11:51:30 -0500

    Add navbar and Event scopes

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :event_attendings, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, through: :event_attendings, source: :event_attendee
+
+  scope :past, -> { where('date < ?', DateTime.now) }
+  scope :future, -> { where('date > ?', DateTime.now) }
 end
